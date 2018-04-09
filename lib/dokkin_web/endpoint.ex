af0f5adbd86ef1c27d701863fbc47bd2a4e87713
mattnguyen1 <mattnguyen1@yahoo.com>
2018-04-08 23:48:53 -0700

    Enable gzip

--- lib/dokkin_web/endpoint.ex
+++ lib/dokkin_web/endpoint.ex
@@ -8,7 +8,7 @@ defmodule DokkinWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :dokkin, gzip: false,
+    at: "/", from: :dokkin, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
