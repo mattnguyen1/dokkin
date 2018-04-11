bdb58f98939b2be169d9bc0fae702de73fcd8fcd
mattnguyen1 <mattnguyen1@yahoo.com>
2018-04-10 23:52:13 -0700

    Update card page UI and make UI more mobile friendly

--- static/src/js/nav-bar/index.js
+++ static/src/js/nav-bar/index.js
@@ -9,10 +9,12 @@ class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
-        <Link to="/" className="logo-link">
-          <img src="https://static.dokk.in/dokkin-logo.png" title="dokk.in"/>
-        </Link>
-        <SearchInput/>
+        <div className="nav-bar-left">
+          <Link to="/" className="logo-link">
+            <img src="https://static.dokk.in/dokkin-logo.png" title="dokk.in"/>
+          </Link>
+          <SearchInput/>
+        </div>
       </div>
     )
   }
